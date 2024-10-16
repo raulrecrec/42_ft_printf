4d81e63d04f5993a16655b6446653497a6e362dd
Raul VM <raulrecrec@gmail.com>
2024-10-16 18:40:59 +0200

    ft_printf.h created with needed libraries

---
+++ ft_printf.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/23 17:08:58 by rexposit          #+#    #+#             */
+/*   Updated: 2024/10/16 18:38:06 by rexposit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stddef.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include "../42_Libft/libft.h"
+
+int		ft_printf(const char *format, ...);
+void	ft_format(const char c, va_list value, int *cont);
+int		ft_putchar(char c);
+void	ft_printf_s(char *s);
+
+#endif
