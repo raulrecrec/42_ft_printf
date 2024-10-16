6e03b8849f0f6679f93f1e40d8658edf59b591a9
Raul VM <raulrecrec@gmail.com>
2024-10-16 19:55:34 +0200

    library libft added

--- ft_printf.c
+++ ft_printf.c
@@ -6,16 +6,16 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 14:59:25 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/15 19:06:24 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/16 19:51:16 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include "ft_printf.h"
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	aargs;
-	size_t	cont;
+	int		cont;
 	size_t	i;
 	
 	va_start(aargs, format);
@@ -26,7 +26,7 @@ int	ft_printf(const char *format, ...)
 		if (format[i] != '%')
 			cont = cont + ft_putchar(format[i]);
 		else
-			ft_type(format[i + 1], aargs, &cont);
+			ft_format(format[i + 1], aargs, &cont);
 		i++;
 	}
 	va_end(aargs);
