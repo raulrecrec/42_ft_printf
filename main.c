a03196e09f020a197aeaaf07a8b630e039fc0e63
Raul VM <raulrecrec@gmail.com>
2024-10-17 22:54:39 +0200

    added new test and original output of printf to check if it works correctly

--- main.c
+++ main.c
@@ -6,16 +6,25 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 18:54:34 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/16 19:47:07 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/17 17:58:49 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
+#include <stdio.h>
 int main(void)
 {
-    ft_printf("String: %S" "Hola, mundo!\n");
+	int cont;
+	char *a = "Hola, mundo!";
+    ft_printf("String: %s\n", a);
     ft_printf("El número es: %d\n", 42);
     ft_printf("Caracter: %c\n", 'A');
+
+	cont = printf("String: %s\n", a);
+	printf("%d\n", cont);
+    cont = printf("El número es: %d\n", 42);
+	printf("%d\n", cont);
+    cont = printf("Caracter: %c\n", 'A');
+	printf("%d\n", cont);
     return 0;
 }
