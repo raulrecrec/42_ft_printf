18849d5928505f331389a457017c270647322f4a
Raul VM <raulrecrec@gmail.com>
2024-10-17 22:51:37 +0200

    added functionalities for ints and working on counting the characters output for every format

--- ft_format.c
+++ ft_format.c
@@ -6,20 +6,32 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 19:07:09 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/16 19:49:47 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/17 19:13:17 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
+#include <stdio.h>
 void	ft_format(const char c, va_list value, int *cont)
 {
 	if (c == 'c')
-		{
-			cont++;
-			ft_putchar(va_arg(value, int));
-		}
+	{
+		ft_putchar_fd(va_arg(value, int), 1);
+		(*cont)++;
+		printf("\n%d\n", *cont);
+	}
 	else if (c == 's')
-		ft_putstr_fd(va_arg(value, char *), 1);
-	
+	{
+		char *str = va_arg(value, char *);
+		ft_putstr_fd(str, 1);
+		*cont = (*cont) + ft_strlen(str);
+		printf("\n%d\n", *cont);
+	}
+	else if (c == 'd')
+	{
+		int num = va_arg(value, int);
+		ft_putnbr_fd(num, 1);
+		*cont = (*cont) + ft_numlen(num);
+		printf("\n%d\n", *cont);
+	}
 }
