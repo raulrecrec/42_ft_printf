4d8c71e91ab501ee63e612688166b2589606902e
Raul VM <raulrecrec@gmail.com>
2024-10-17 22:54:04 +0200

    delete unnecessary function and ft_numlen added

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 17:08:58 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/16 19:34:30 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/17 22:53:28 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@
 
 int		ft_printf(const char *format, ...);
 void	ft_format(const char c, va_list value, int *cont);
-int		ft_putchar(char c);
+int		ft_numlen(int c);
 
 #endif
