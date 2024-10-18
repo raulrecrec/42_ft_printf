a067d994ee743dbef1e5167a2c8fb8f47164dca6
Raul VM <raulrecrec@gmail.com>
2024-10-18 20:39:26 +0200

    tester updated with %i and %d, also output clarity added

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 18:54:34 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/17 17:58:49 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/18 20:16:29 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,22 @@ int main(void)
 {
 	int cont;
 	char *a = "Hola, mundo!";
-    ft_printf("String: %s\n", a);
-    ft_printf("El número es: %d\n", 42);
-    ft_printf("Caracter: %c\n", 'A');
+    cont = ft_printf("String: %s\n", a);
+	printf("%d\n", cont);
+    cont = ft_printf("El número es: %d\n", 42);
+	printf("%d\n", cont);
+    cont = ft_printf("Caracter: %c\n", 'A');
+	printf("%d\n", cont);
+    cont = ft_printf("El número es: %i\n", 423);
+	printf("%d\n\nPRINTF ORIGINAL:\n\n", cont);
 
 	cont = printf("String: %s\n", a);
 	printf("%d\n", cont);
     cont = printf("El número es: %d\n", 42);
 	printf("%d\n", cont);
     cont = printf("Caracter: %c\n", 'A');
+	printf("%d\n", cont);
+    cont = printf("El número es: %i\n", 423);
 	printf("%d\n", cont);
     return 0;
 }
