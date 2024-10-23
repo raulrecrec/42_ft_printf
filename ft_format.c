73a1d3fc795fe0c9bead6b2fb714cc914d00af06
Raul VM <raulrecrec@gmail.com>
2024-10-23 14:24:05 +0200

    norm correction

--- ft_format.c
+++ ft_format.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 19:07:09 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/23 14:20:29 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/23 14:23:50 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,21 @@
 
 int	ft_format(const char c, va_list value)
 {
-	int	printed_chars;
+	int	print_chars;
 
 	if (c == 'c')
-	{
-		printed_chars = ft_putlchar(va_arg(value, int));
-	}
+		print_chars = ft_putlchar(va_arg(value, int));
 	else if (c == 's')
-	{
-		printed_chars = ft_putlstr(va_arg(value, char *));
-	}
+		print_chars = ft_putlstr(va_arg(value, char *));
 	else if (c == 'd' || c == 'i')
-	{
-		printed_chars = ft_putlnbr(va_arg(value, int));
-	}
+		print_chars = ft_putlnbr(va_arg(value, int));
 	else if (c == 'x' || c == 'X')
-	{
-		printed_chars = ft_putlnbr_base_xup(c, va_arg(value, long long));
-	}
+		print_chars = ft_putlnbr_base_xup(c, va_arg(value, long long));
 	else if (c == 'u')
-	{
-		printed_chars = ft_putlnbr_base_xup(c, va_arg(value, unsigned int));
-	}
+		print_chars = ft_putlnbr_base_xup(c, va_arg(value, unsigned int));
 	else if (c == '%')
-	{
-		printed_chars = ft_putlchar('%');
-	}
+		print_chars = ft_putlchar('%');
 	else if (c == 'p')
-	{
-		printed_chars = ft_putlnbr_base_xup(c, va_arg(value, unsigned long long));
-	}
-	return (printed_chars);
+		print_chars = ft_putlnbr_base_xup(c, va_arg(value, unsigned long long));
+	return (print_chars);
 }
