e926b1bc9a2e20f7366eb2ac9b47220943fcfe5f
Raul VM <raulrecrec@gmail.com>
2024-10-23 13:47:32 +0200

    tests for %% format added

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: rexposit <rexposit@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 18:54:34 by rexposit          #+#    #+#             */
-/*   Updated: 2024/10/23 13:20:26 by rexposit         ###   ########.fr       */
+/*   Updated: 2024/10/23 13:44:58 by rexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ int main(void)
     cont = ft_printf("El número hex es: %X\n", 255);
 	printf("%d\n", cont);
     cont = ft_printf("El número unsigned es: %u\n", 42);
+	printf("%d\n", cont);
+    cont = ft_printf("El procentaje de 42 es: %d%%\n", 42);
 	printf("%d\n\nPRINTF ORIGINAL:\n\n", cont);
 
 	cont = printf("String: %s\n", a);
@@ -44,6 +46,8 @@ int main(void)
     cont = printf("El número hex es: %X\n", 255);
 	printf("%d\n", cont);
     cont = printf("El número unsigned es: %u\n", 42);
+	printf("%d\n", cont);
+    cont = printf("El procentaje de 42 es: %d%%\n", 42);
 	printf("%d\n", cont);
     return 0;
 }
